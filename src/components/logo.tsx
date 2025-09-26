7f81733a2ce8d7c125ea1ae23e63377fc2dc51ee
anguo liu <angusliu9898@gmail.com>
2025-09-26 14:35:15 +0000

    请全部更改为这个形状的LOGO

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,20 +1,15 @@
-import { cn } from "@/lib/utils";
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("size-8", className)}
-    >
-      <path d="M4 6h16" />
-      <path d="M4 12h16" />
-      <path d="M4 18h7" />
-    </svg>
+    <div className={cn("relative h-8 w-8", className)}>
+      <Image
+        src="/logo.png"
+        alt="Leverage Platform Logo"
+        layout="fill"
+        objectFit="contain"
+      />
+    </div>
   );
 }
