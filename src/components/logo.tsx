b2dfdb3e81b65930eea797e02c20476070b1d7b5
anguo liu <angusliu9898@gmail.com>
2025-09-26 14:46:32 +0000

    我认为还是不用图片格式的LOGO了，改回你之前的方案，重新设计一个科技感强的。

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,23 @@
-import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative h-8 w-auto aspect-[220/63]", className)}>
-      <Image
-        src="/logo.png"
-        alt="Leverage Platform Logo"
-        layout="fill"
-        objectFit="cover"
-        className="rounded-sm"
-      />
+    <div className={cn("relative h-8 w-8", className)}>
+      <svg
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-full h-full"
+      >
+        <path
+          d="M20 80 L50 20 L80 80 L70 80 L50 40 L30 80 Z"
+          className="fill-primary"
+        />
+        <path
+          d="M35 70 L50 50 L65 70 L50 70 Z"
+          className="fill-accent"
+        />
+      </svg>
     </div>
   );
 }
