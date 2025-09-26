cf636578193f2052bf047a6ae969cc940949bf08
anguo liu <angusliu9898@gmail.com>
2025-09-26 14:38:03 +0000

    这个图片不是矢量图，显示不正常，请改一下像素，再使用。

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,13 @@ import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative h-8 w-8", className)}>
+    <div className={cn("relative h-8 w-auto aspect-[220/63]", className)}>
       <Image
         src="/logo.png"
         alt="Leverage Platform Logo"
         layout="fill"
-        objectFit="contain"
+        objectFit="cover"
+        className="rounded-sm"
       />
     </div>
   );
