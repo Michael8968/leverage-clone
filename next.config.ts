d03c361c98457ebe258f0c0b59a8d639bc260b27
anguo liu <angusliu9898@gmail.com>
2025-09-28 12:28:03 +0000

    Try fixing this error: `Console Error: Error: Firebase Admin initializat

--- next.config.ts
+++ next.config.ts
@@ -54,6 +54,13 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_SERVICE_ACCOUNT_KEY: process.env.FIREBASE_SERVICE_ACCOUNT_KEY,
   },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
