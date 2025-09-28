fd82049ec5efee44fe255493ad475581ffe0fedd
anguo liu <angusliu9898@gmail.com>
2025-09-28 12:29:46 +0000

    请用中文回复

--- next.config.ts
+++ next.config.ts
@@ -61,6 +61,14 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    FIREBASE_SERVICE_ACCOUNT_KEY: process.env.FIREBASE_SERVICE_ACCOUNT_KEY,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  },
 };
 
 export default nextConfig;
